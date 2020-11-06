fd155f5c10af9474d83f06249c4edfe595b78b10
Abdellatif Elaroussi <elaroussi@outlook.com>
2020-11-06 14:29:17 +0100

    Update navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,9 +13,9 @@ export default function Navbar() {
 				<li>
 					<Link to='/about'>About</Link>
 				</li>
-				<li>
+				{/* <li>
 					<Link to='/contact'>Contact</Link>
-				</li>
+				</li> */}
 			</ul>
 		</nav>
 	);
